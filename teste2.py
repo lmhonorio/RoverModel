3be201b85954e571f73810ed62326efb0a91704b
gabrielle <gabrielle.timotheo@engenharia.ufjf.br>
2025-10-21 13:27:35 -0300

    unnecessary files

--- teste2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from movns_ains_argo import send_mission_argo
-
-# Tem que colocar ponto inicial, senão não funciona
-send_mission_argo.generate_mission([[-3.1231947, -41.7653947],[-3.1232803141546217, -41.76542310046668]], 1)
-print(f"✅ Missão enviada para robô {1}")
